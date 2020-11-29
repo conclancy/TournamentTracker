00f0003210ce60999a03965326daa838a8123968
conclancy <conclancy@hotmail.com>
2020-11-29 17:22:39 -0500

    Created TextFileProcessor helper class

---
+++ TrackerLibrary/DataConnections/TextFileProcessor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Linq;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataConnections.TextHelpers
+{
+    public static class TextFileProcessor
+    {
+        /// <summary>
+        /// Generates the full file path given a file name.
+        /// </summary>
+        /// <param name="fileName">The name of the file</param>
+        /// <example>PrizeModel.csv</example>
+        public static string FullFilePath(this string fileName)
+        {
+            return $"{ ConfigurationManager.AppSettings["filePath"] }\\{ fileName }";
+        }
+
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<String>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
+
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+
+            foreach (string l in lines)
+            {
+                string[] cols = l.Split(',');
+
+                PrizeModel p = new PrizeModel();
+                p.Id = int.Parse(cols[0]);
+                p.PlaceNumber = int.Parse(cols[1]);
+                p.PlaceName = cols[2];
+                p.PrizeAmount = decimal.Parse(cols[3]);
+                p.PrizePercentage = double.Parse(cols[4]);
+
+                output.Add(p);
+            }
+
+            return output;
+        }
+
+        public static void SaveToPrizeFile (this List<PrizeModel> models, string fileName)
+        {
+            List<string> lines = new List<string>();
+
+            foreach(PrizeModel p in models)
+            {
+                lines.Add($"{p.Id},{p.PlaceNumber},{p.PlaceName},{p.PrizeAmount},{p.PrizePercentage}");
+            }
+
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+    }
+}
