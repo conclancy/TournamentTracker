d70021f073b8f01ae1d4ff0d6bf7255dfc9cea89
conclancy <conclancy@hotmail.com>
2020-11-29 17:23:20 -0500

    Created text database for PrizeModel

--- TrackerLibrary/DataConnections/TextConnector.cs
+++ TrackerLibrary/DataConnections/TextConnector.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataConnections.TextHelpers;
+using System.Linq;
 
 namespace TrackerLibrary.DataConnections
 {
     public class TextConnector : IDataConnection
     {
-        // TODO - Wire up the CreatePrize for text files.
+        private const string PrizesFile = "PrizeModels.csv";
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // load the txt file and convert text file to a list<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // find the id
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            // add the new record with the new PrizeID (max + 1)
+            model.Id = currentId;
+            prizes.Add(model);
+
+            // convert the prizes to a list<string> and save the list<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -19,7 +19,7 @@ namespace TrackerUI
             Application.SetCompatibleTextRenderingDefault(false);
 
             // Initialize the database connections
-            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.Sql);
+            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.TextFile);
 
             // TODO - Switch this back
             Application.Run(new CreatePrizeForm());
