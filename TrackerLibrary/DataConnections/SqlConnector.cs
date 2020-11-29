0813d3bf7cbe0a0657d5d5b801ff285e4581c319
conclancy <conclancy@hotmail.com>
2020-11-29 13:30:16 -0500

    Created TrackerLibrary.DataConnections repo and updated namespace.

--- TrackerLibrary/DataConnections/SqlConnector.cs
+++ TrackerLibrary/DataConnections/SqlConnector.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Models;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.DataConnections
 {
     public class SqlConnector : IDataConnection
     {
